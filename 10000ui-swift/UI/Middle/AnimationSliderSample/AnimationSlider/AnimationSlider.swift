3f0cd01606e3d7e93d75d9f299769940781513e9
blurryssky <blurryssky@icloud.com>
2018-11-27 10:28:43 +0800

    fix pulling header gif

---
+++ 10000ui-swift/UI/Middle/AnimationSliderSample/AnimationSlider/AnimationSlider.swift
@@ -0,0 +1,21 @@
+//
+//  AnimationSlider.swift
+//  10000ui-swift
+//
+//  Created by blurryssky on 2018/5/1.
+//  Copyright © 2018年 blurryssky. All rights reserved.
+//
+
+import UIKit
+
+class AnimationSlider: UIControl {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ 10000ui-swift/UI/Simple/RectFillImageViewSample/RectFillImageViewSampleViewController.swift
@@ -0,0 +1,35 @@
+//
+//  RectFillImageViewSampleViewController.swift
+//  10000ui-swift
+//
+//  Created by blurryssky on 2018/5/2.
+//  Copyright © 2018年 blurryssky. All rights reserved.
+//
+
+import UIKit
+
+class RectFillImageViewSampleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
